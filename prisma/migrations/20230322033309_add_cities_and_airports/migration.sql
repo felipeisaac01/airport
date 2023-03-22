f753f839305e588fb3a58e0785ec8d613e249503
Felipe Isaac <felipeisaacferreira@gmail.com>
2023-03-22 00:38:28 -0300

    feat(airports): added migration

---
+++ prisma/migrations/20230322033309_add_cities_and_airports/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "federative_units" AS ENUM ('BA', 'SP', 'RJ', 'MG', 'RN', 'RS');
+
+-- CreateTable
+CREATE TABLE "cities" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+    "name" TEXT NOT NULL,
+    "federativeUnit" "federative_units" NOT NULL,
+
+    CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "airports" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+    "cityId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "iataCode" TEXT NOT NULL,
+
+    CONSTRAINT "airports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "airports_iataCode_key" ON "airports"("iataCode");
+
+-- AddForeignKey
+ALTER TABLE "airports" ADD CONSTRAINT "airports_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "cities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
