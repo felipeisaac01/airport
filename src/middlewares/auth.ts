6d93eafb4581a385c00a9e9b23cadba90ed9af75
Felipe Isaac <felipeisaacferreira@gmail.com>
2023-03-22 16:10:47 -0300

    fix(middleware): fixed middleware tipe mistake

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,16 +1,14 @@
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import { IAuthTokenDto } from "../domain/dtos/controllers/auth";
 import { UnauthorizedError } from "../errors/apiErrors";
 
 export async function authenticate(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization
+    const token = req.headers.authorization
 
-    if (!authHeader) {
+    if (!token) {
         throw new UnauthorizedError("Authorization header missing", "MW-01")
     }
-
-    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = verify(token, process.env.JWT_TOKEN ?? "/") as IAuthTokenDto;
