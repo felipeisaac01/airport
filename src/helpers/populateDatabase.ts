96247cd1716cd88baf05cddd6ac248d04cb20c26
Felipe Isaac <felipeisaacferreira@gmail.com>
2023-03-22 01:00:07 -0300

    chore: added helper function to populate database

---
+++ src/helpers/populateDatabase.ts
@@ -0,0 +1,100 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function populateDatabase() {
+    const prismaClient = new PrismaClient()
+    
+    const existingAirports = await prismaClient.airport.findMany()
+
+    if (!existingAirports.length) {
+        await prismaClient.city.createMany({
+            data: [
+                {
+                    federativeUnit: "BA",
+                    name: "Salvador",
+                },
+                {
+                    federativeUnit: "MG",
+                    name: "Belo Horizonte"
+                },
+                {
+                    federativeUnit: "RJ",
+                    name: "Rio de Janeiro"
+                },
+                {
+                    federativeUnit: "RN",
+                    name: "Natal"
+                },
+                {
+                    federativeUnit: "RS",
+                    name: "Porto Alegre"
+                },
+                {
+                    federativeUnit: "SP",
+                    name: "São Paulo"
+                }
+            ]
+        })
+
+        const cities = await prismaClient.city.findMany()
+
+        const airportsToInsert: {
+            cityId: string,
+            iataCode: string,
+            name: string
+        }[] = [];
+
+        for (const city of cities) {
+            if (city.federativeUnit === "BA") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "SSA",
+                    name: "Dois de Julho"
+                })
+            } else if (city.federativeUnit === "MG") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "BHZ",
+                    name: "Confins"
+                })
+            } else if (city.federativeUnit === "RJ") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "GIG",
+                    name: "Galeão"
+                }, {
+                    cityId:  city.id,
+                    iataCode: "SDU",
+                    name: "Santos Dummont"
+                })
+            } else if (city.federativeUnit === "SP") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "GRU",
+                    name: "Guarulhos"
+                }, {
+                    cityId:  city.id,
+                    iataCode: "CGH",
+                    name: "Congonhas"
+                })
+            } else if (city.federativeUnit === "RN") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "NAT",
+                    name: "Governador Aluízio Alves"
+                })
+            } else if (city.federativeUnit === "RS") {
+                airportsToInsert.push({
+                    cityId:  city.id,
+                    iataCode: "POA",
+                    name: "Salgado Filho"
+                })
+            }
+        }
+
+        await prismaClient.airport.createMany({
+            data: airportsToInsert
+        })
+
+        prismaClient.$disconnect()
+    }
+}
